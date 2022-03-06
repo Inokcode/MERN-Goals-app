8850b6b8932baf3d514575ba6d113b7e6b603a03
Inokcode <regilearn@gmail.com>
2022-03-06 17:06:01 +0530

    Frontend UI added

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ yarn add express-async-handler
 
 yarn add bcryptjs
 yarn add jsonwebtoken 
+
+yarn add react-router-dom
+yarn add react-icons
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa';
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return <>435
+  <header className='header'>
+    <div className='logo'><Link to='/'>GoalSetter</Link></div>
+    <ul>
+      <li>  <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link></li>
+              <li></li>
+              <li>  <Link to='/register'>
+                <FaUser /> Register
+              </Link></li>
+              <li></li>
+    </ul>
+  </header>
+  </>
+}
+
+export default Header
