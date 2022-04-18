0b7fdd9ec4c0794356eaadac7f7a5e34ca2b8a15
Inokcode <regilearn@gmail.com>
2022-04-18 15:47:16 +0530

    test

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 function Spinner() {
   return (
     <div className="loadingSpinnerContainer">
-      <div className="loadingSpinner">test 3</div>
+      <div className="loadingSpinner">test 4</div>
     </div>
   );
 }
